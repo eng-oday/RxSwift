4ffefe0cc23cf4c368fda6c83ea64e9885813a36
eng-oday <odaydieg@gmail.com>
2023-12-16 21:23:20 +0200

    apply on Take and distinct operators

--- RxSwift-Demo/Ch5-Filtering Operator/Filtering_Operator.swift
+++ RxSwift-Demo/Ch5-Filtering Operator/Filtering_Operator.swift
@@ -11,10 +11,7 @@ import RxSwift
 
 class Filtering_Opertaor{
     
-    let publish_Subject_ignore_Element     = PublishSubject<String>()
-    let elemenAt_Puclish_Subject           = PublishSubject<String>()
     let disposeBag                         = DisposeBag()
-    
     
     func exampleOn_Filtering_Opertaor(){
         print("==================== Filtering_Opertaor ====================")
@@ -26,10 +23,15 @@ class Filtering_Opertaor{
         run_Skip()
         run_Skip_while()
         run_Skip_Until()
-        
+        run_Take()
+        run_Take_While()
+        run_Take_Until()
+        run_Distinict_Until_Changed()
+        run_Distinict_Until_Changed_Custom_Comapring()
     }
     private func run_Ignore_Elements(){
         print("==================== ignore_Element ====================")
+        let publish_Subject_ignore_Element     = PublishSubject<String>()
         publish_Subject_ignore_Element
             .ignoreElements()
             .subscribe { element in
@@ -46,6 +48,7 @@ class Filtering_Opertaor{
     
     private func run_elementAt(){
         print("==================== element_At ====================")
+        let elemenAt_Puclish_Subject           = PublishSubject<String>()
         elemenAt_Puclish_Subject
             .element(at: 2)
             .subscribe { element in
@@ -105,8 +108,61 @@ class Filtering_Opertaor{
         publishSubject.onNext("B")
         trigger.onNext("Hi")
         publishSubject.onNext("C")
+    }
+    
+    private func run_Take(){
+        
+        // What you take is what you get
+        print("==================== Take ====================")
+        Observable.of(1,2,3,4,5,6,7)
+            .take(4) //take four element so in print i will get first four element
+            .subscribe { element in
+            print(element)
+        }.disposed(by: disposeBag)
         
         
     }
+    
+    private func run_Take_While(){
+        print("==================== Take_While ====================")
+        Observable.of(2,6,4,4,6,6)
+            .enumerated()
+            .take(while: { (index: Int, element: Int) in
+                // index > 1 -> here it will not return any value because the condition it fail , and condition if fail it will terminate
+                index < 1
+            }).map{
+                $0.element
+            }
+            .subscribe { element in
+                print(element)
+            }.disposed(by: disposeBag)
+        
+    }
 
+    private func run_Take_Until(){
+        print("==================== Take_Until ====================")
+
+        let pusblish_Subject    = PublishSubject<Int>()
+        let trigger             = PublishSubject<Int>()
+        pusblish_Subject.take(until: trigger).subscribe { element in
+            print(element)
+        }.disposed(by: disposeBag)
+        
+        pusblish_Subject.onNext(1)
+        pusblish_Subject.onNext(2)
+        trigger.onNext(3)
+        pusblish_Subject.onNext(4)
+
+    }
+    
+    private func run_Distinict_Until_Changed(){
+        print("==================== Distinict_Until_Changed ====================")
+        
+        Observable.of(1,1,2,2,3,4,4,5,6,3)
+            .distinctUntilChanged()
+            .subscribe { element in
+                print(element)
+            }.disposed(by: disposeBag)
+
+    }
 }
